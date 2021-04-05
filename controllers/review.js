1c1c23224817ba511bca74bbe9198c7d318fa53e
suhyepark98 <suhyepark98@gmail.com>
2021-04-05 11:01:02 -0400

    update

--- controllers/review.js
+++ controllers/review.js
@@ -11,34 +11,34 @@ module.exports = (app) => {
 
     app.post('/review/:id', (req, res) => {
         async.waterfall([
-            (callback) => {
+            function (callback) {
                 Organization.findOne({ '_id': req.params.id }, (err, result) => {
                     callback(err, result);
                 })
             },
 
-            (result, callback) => {
-                Organization.update({
+            function (result, callback) {
+                Organization.updateOne({
                     '_id': req.params.id
-                }),
-                {
-                    $push: {
-                        organizationRating: {
-                            organizationName: req.body.sender,
-                            userFullname: req.user.fullname,
-                            userRole: req.user.role,
-                            organizationImage: req.user.organization.image,
-                            userRating: req.body.clickedValue,
-                            userReview: req.body.review
+                },
+                    {
+                        $push: {
+                            organizationRating: {
+                                organizationName: req.body.sender,
+                                userFullname: req.user.fullname,
+                                userRole: req.user.role,
+                                organizationImage: req.user.organization.image,
+                                userRating: req.body.clickedValue,
+                                userReview: req.body.review
+                            },
+                            ratingNumber: req.body.clickedValue
                         },
-                        ratingNumber: req.body.clickedValue
-                    },
-                    // increment the sum by the clicked value
-                    $inc: { ratingSum: req.body.clickedValue }
-                }, (err) => {
-                    req.flash('success', 'Your review / rating has been successfully submitted.');
-                    req.redirect('/review/' + req.params.id);
-                }
+                        // increment the sum by the clicked value
+                        $inc: { ratingSum: req.body.clickedValue }
+                    }, (err) => {
+                        req.flash('success', 'Your review / rating has been successfully submitted.');
+                        res.redirect('/review/' + req.params.id);
+                    })
             }
         ])
     });
