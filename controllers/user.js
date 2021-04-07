171ee7fcc5d8468636022ca3d118204a80b89f92
Moon Hwan <moon_hwan_kim@brown.edu>
2021-04-07 10:58:45 +0900

    Final Push/Database
    1

--- controllers/user.js
+++ controllers/user.js
@@ -55,7 +55,7 @@ module.exports = (app) => {
                 res.redirect('/organizationprofile/'+data[0]._id);
             }
             catch(err) {
-                console.log(err);
+                console.log(err)
             }
             
         });
